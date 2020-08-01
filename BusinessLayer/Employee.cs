19e7f518a672779f58847cfbac49e1b956e420d3
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 17:12:42 +0530

    MvcAppli2

---
+++ BusinessLayer/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessLayer
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string DateOfBirth { get; set; }
+    }
+}
---
+++ MvcAppli2/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace MvcAppli2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+            return View(employees);
+        }
+
+        [HttpGet]
+        [ActionName("Create")]
+        public ActionResult Create_Get() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_Post()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer =
+                    new EmployeeBusinessLayer();
+            Employee employee = new Employee();
+            TryUpdateModel(employee);
+            if (ModelState.IsValid)
+            {
+                employeeBusinessLayer.AddEmmployee(employee);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }            
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id) {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            Employee employee = employeeBusinessLayer.Employees.Single(emp=>emp.ID==id);
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            employeeBusinessLayer.SaveEmployee(employee);
+
+            return View(employee);
+        }
+    }
+}
